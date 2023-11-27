901e1f2ea8f889d4b7ab8ceac1aee55f69318bf9
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-11-27 11:18:17 +0200

    change basename in BroserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/car-rental">
+    <BrowserRouter basename="/react-tailwindcss-car-rental">
       <App />
     </BrowserRouter>
   </React.StrictMode>
